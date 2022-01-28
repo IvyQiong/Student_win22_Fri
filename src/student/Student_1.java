51fa39ed8019da3b2e254aa29ae31fcdea52cbe8
IvyQiong <98172790+IvyQiong@users.noreply.github.com>
2022-01-28 09:48:32 -0500

    Added comments

--- src/student/Student_1.java
+++ src/student/Student_1.java
@@ -1,6 +1,6 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ Author: Qiong Liao
+ Date: jan 28th 2022
  */
 package student;
 
